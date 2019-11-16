88de724896c9e2dcd468cefab5b49d0482170da7
Ken Swain <ken@kenswain.com>
2019-11-16 13:49:49 -0600

    Added safe scan and full scan. Still have some clean up to do for better reporting.

--- pitboss.py
+++ pitboss.py
@@ -12,25 +12,27 @@ from modules.write_csv import *
 import csv
 
 
-
+"""
 ROOT_DIR = "Targets"
 creat_dir(ROOT_DIR)
-
+"""
 #Setting up parsers for arguments
 parser = argparse.ArgumentParser("Looking for command line arguments of domain names")
 parser.add_argument('-d', default='None', type=str, help='The is the full domain name you are looking up')
 parser.add_argument('-t', default='None', type=str, help='This is the target name.')
+parser.add_argument('-s', default="Full", type=str, help="Chose scanning type. Either Full or Safe")
 
 args = parser.parse_args()
 full_domain = args.d
 target = args.t
-if full_domain.startswith("http://"):
+scan = args.s 
+
+if full_domain.startswith("http://") or full_domain.startswith("https://"):
     full_domain = full_domain
 else:
     full_domain = "http://" + full_domain
 
-def gather_info(full_url):
-    creat_dir(ROOT_DIR)
+def full_scan(full_url):
     print("Getting IP Address")
     print("")
     ipaddress = get_address(full_url)
@@ -59,21 +61,34 @@ def gather_info(full_url):
     print("")
     sitestatus = get_site_status(full_url)
     report(ipaddress, domainname, robots_txt, nmap, whois, outdated, sitestatus)
-    
+
+def safe_scan(full_url):
+   
+    print("Getting IP Address")
+    print("")
+    ipaddress = get_address(full_url)
+    print("")
+    print("Getting Domain Name")
+    print("")
+    domainname = get_domain_name(full_url)
+    print("")
+    whois = ''
+    robots_txt = ''
+    nmap = ''
+    outdated = ''
+    print("Checking site status")
+    print("")
+    sitestatus = get_site_status(full_url)
+    report(ipaddress, domainname, robots_txt, nmap, whois, outdated, sitestatus)    
+   
 def report(ipaddress, domainname, robots_txt, nmap, whois, outdated, sitestatus):
-    project_dir = ROOT_DIR + "/" + target
-    creat_dir(project_dir)
-    write_file(project_dir + "/ipaddress.txt", ipaddress)
-    write_file(project_dir + "/domainname.txt", domainname)
-    write_file(project_dir + "/robots.txt", robots_txt)
-    write_file(project_dir + "/whois.txt", whois)
-    write_file(project_dir + "/nmap.txt", nmap)
-    write_file(project_dir + "/outdated.txt", outdated)
-    write_file(project_dir + "/status.txt", sitestatus)
     checkfile()
     write_findings(target, domainname, ipaddress, sitestatus, whois, robots_txt, outdated, nmap)
 
 if full_domain != 'None' and target != 'None':
-    gather_info(full_domain)
+    if args.s == 'Full':
+        full_scan(full_domain)
+    elif args == 'Safe' or 's':
+        safe_scan(full_domain)
 else:
     print("You must enter both a target and FQDN to start scan.", "Please use --help for more information", sep="\n\n")
